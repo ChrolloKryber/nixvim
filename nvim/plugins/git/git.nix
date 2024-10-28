172cbebde5486b2c1a05a1c98135897d86a646bc
ChrolloKryber <rishay.raghav00@gmail.com>
2024-10-28 19:40:04 +0530

    Add lazygit
    added lazygit enable and bound keymap for launching it

--- nvim/plugins/git/git.nix
+++ nvim/plugins/git/git.nix
@@ -1,5 +1,18 @@
 {
   plugins = {
     gitsigns.enable = true;
+    lazygit.enable = true;
   };
+
+  keymaps = [
+    {
+      mode = ["n"];
+      key = "<A-g>";
+      action = "<cmd>LazyGit<CR>";
+      options = {
+        silent = true;
+        desc = "Open a LazyGit window";
+      };
+    }
+  ];
 }
