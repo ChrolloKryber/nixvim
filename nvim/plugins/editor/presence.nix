8f957eaca6b182bf9a349efff37d6ab454acc97a
ChrolloKryber <rishay.raghav00@gmail.com>
2024-10-31 16:48:49 +0530

    update presence buttons
    how not to show the repo you are working on

--- nvim/plugins/editor/presence.nix
+++ nvim/plugins/editor/presence.nix
@@ -5,5 +5,11 @@
     showTime = false;
     workspaceText = "Working on a project";
     neovimImageText = "Make the jump from VS Code my friend!";
+    buttons = [
+      {
+        label = "YouTube";
+        url = "https://www.youtube.com/watch?v=08xzc6pZ0m8";
+      }
+    ];
   };
 }
